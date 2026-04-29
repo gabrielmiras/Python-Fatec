56533caa39697c0bb565c33da9635c9e7a0fbd88
GABRIEL ZIFIRINO DE MIRANDA <1051392611027@fatec.corp>
2026-04-29 09:37:36 -0300

    -

---
+++ Vetores lista/Vetores lista 1.py
@@ -0,0 +1,9 @@
+numeros = [1,2,3,4,5]
+
+for i in range(5):
+    if numeros[i] % 2 ==0:
+        print(f"{numeros[i]} Par")
+
+    num=int(input("Digite um número:"))
+    if num  % 2 == 0:
+        print("par")
